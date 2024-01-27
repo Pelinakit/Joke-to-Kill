7375b353edc1501fe44c6d77299446f811cac437
suskikoodaa <101000884+suskikoodaa@users.noreply.github.com>
2024-01-27 18:38:31 +0200

    Add script to jousi and projetiles

--- Assets/JousiController.cs
+++ Assets/JousiController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class JousiController : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float projectileSpeed = 10f;
+    
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void Shoot()
     {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
         
+        if (projectileRb != null)
+        {
+            projectileRb.AddForce(Vector3.up * projectileSpeed, ForceMode.VelocityChange);
+        }
     }
 }
