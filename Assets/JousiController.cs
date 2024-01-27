d681bb1f6ac8612f1a8c3ea7178716d54bfdab36
jabbahut69 <155767845+jabbahut69@users.noreply.github.com>
2024-01-27 23:34:11 +0200

    Add bow rotation, add potato anim

--- Assets/JousiController.cs
+++ Assets/JousiController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class JousiController : MonoBehaviour
@@ -7,44 +5,50 @@ public class JousiController : MonoBehaviour
     public GameObject projectilePrefab;
     public Transform firePoint;
     public float projectileSpeed = 10f;
-    
-    // Start is called before the first frame update
+
     void Update()
     {
+        // Get the mouse position in screen space
+        Vector2 mousePos = Input.mousePosition;
+
+        // Convert the mouse position to a point in the game world
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(new Vector2(mousePos.x, mousePos.y));
+
+        // Calculate the direction from the crossbow to the mouse cursor
+        Vector2 direction = worldPos - (Vector2)transform.position;
+
+        // Calculate the rotation angle in degrees
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+
+        // Rotate the crossbow to face the mouse cursor
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Shoot();
         }
-
-        // Get the mouse position in screen space
-        Vector3 mousePos = Input.mousePosition;
-
-        // Convert the mouse position to a point in the game world
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f)); // Assuming the camera is at a Z distance of 10
-
-        // Calculate the direction from the crossbow to the mouse cursor
-        Vector3 direction = worldPos - transform.position;
-
-        // Calculate the rotation angle in degrees
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // Rotate the crossbow to face the mouse cursor
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        // Optionally, rotate the tip of the crossbow independently
-        // tip.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
-    // Update is called once per frame
     void Shoot()
     {
-        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        // Offset the rotation by 270 degrees to make the arrow move upward
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation * Quaternion.Euler(0, 0, 270f));
         Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
-        
+
         if (projectileRb != null)
         {
-            projectileRb.AddForce(Vector2.up * projectileSpeed, ForceMode2D.Impulse);
+            // Calculate the shooting direction based on the rotated firePoint
+            Vector2 shootDirection = new Vector2(Mathf.Cos(Mathf.Deg2Rad * firePoint.eulerAngles.z), Mathf.Sin(Mathf.Deg2Rad * firePoint.eulerAngles.z));
+
+            // Use the calculated direction to determine the shooting direction
+            projectileRb.velocity = shootDirection * projectileSpeed;
         }
+
+        // Attach the ArrowProjectile script to the instantiated arrow
+        projectile.AddComponent<ArrowProjectile>();
     }
+
+
 
 }
