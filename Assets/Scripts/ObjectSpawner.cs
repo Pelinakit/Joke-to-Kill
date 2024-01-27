81f9f4b71a2af51ac2c97076035f0e2ce5c6424d
jabbahut69 <155767845+jabbahut69@users.noreply.github.com>
2024-01-27 20:57:23 +0200

    Add text shit

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ObjectSpawner : MonoBehaviour
 {
@@ -27,16 +28,39 @@ public class ObjectSpawner : MonoBehaviour
             timer = 0;
         }
     }
-   
+
     void SpawnObject()
     {
         if (prefabToSpawn != null && spawnPoint != null)
         {
-            Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
+            GameObject spawnedObject = Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
+
+            // Output information about all children to the debug log
+            Debug.Log("Children of the spawned object:");
+
+            for (int i = 0; i < spawnedObject.transform.childCount; i++)
+            {
+                Transform child = spawnedObject.transform.GetChild(i);
+                Debug.Log("Child " + i + ": " + child.name);
+            }
+
+            // Optionally, find and log specific components of the children
+            TextMeshProUGUI textMeshProComponent = spawnedObject.GetComponentInChildren<TextMeshProUGUI>();
+            if (textMeshProComponent != null)
+            {
+                Debug.Log("TextMeshPro Component found in children: " + textMeshProComponent.text);
+            }
+            else
+            {
+                Debug.LogError("TextMeshPro component not found in the children of the spawned object.");
+            }
         }
         else
         {
-            Debug.LogError("Prefab or spawn point not set in Object Spawner script. ");
+            Debug.LogError("Prefab or spawn point not set in Object Spawner script.");
         }
     }
+
+
+
 }
